9626e26695febc8685ad751fe71a1e7e721d0483
Evert van Brussel <ecvanbrussel@gmail.com>
2021-03-16 07:27:42 +0100

    Add svelte-check

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -1,16 +1,16 @@
-<script lang="ts">
-	let count: number = 0;
+<script>
+	let count = 0;
 
 	const increment = () => {
 		count += 1;
 	};
 </script>
 
-<button on:click={increment} >
+<button on:click={increment}>
 	Clicks: {count}
 </button>
 
-<style lang="postcss">
+<style>
 	button {
 		@apply px-4 py-2 bg-svelte-100 text-svelte-500 border rounded rounded-[60px];
 		font-variant-numeric: tabular-nums;
